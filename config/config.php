f7aad8cf41695a9f137af8455c97bc4847f46ddf
czolnowski <marcin@piwik.pro>
2015-06-08 16:49:11 +0200

    Simplify version of config for logger

--- config/config.php
+++ config/config.php
@@ -7,46 +7,26 @@
  */
 
 use Interop\Container\ContainerInterface;
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Piwik\Log;
 
 return array(
 
-    'Piwik\Plugins\LoginSamlSSO\Logger' => DI\object('Monolog\Logger')
-        ->constructor('loginsamlsso', DI\link('loginsamlsso.log.handlers'), DI\link('log.processors')),
-
-    'loginsamlsso.log.handlers' => DI\factory(function (ContainerInterface $c) {
-        if ($c->has('ini.LoginSamlSSO.log_writers')) {
-            $writerNames = $c->get('ini.LoginSamlSSO.log_writers');
-        } else {
-            return array();
-        }
-        $classes = array(
-            'file'     => 'Piwik\Plugins\Monolog\Handler\FileHandler',
-            'screen'   => 'Piwik\Plugins\Monolog\Handler\WebNotificationHandler',
-            'database' => 'Piwik\Plugins\Monolog\Handler\DatabaseHandler',
+    'Piwik\Plugins\LoginSamlSSO\Logger' => DI\factory(function (ContainerInterface $c) {
+        $handler = new StreamHandler(
+            $c->get('loginsamlsso.log.file.filename'),
+            $c->get('loginsamlsso.log.level')
         );
-        $writerNames = array_map('trim', $writerNames);
-        $writers = array();
-        foreach ($writerNames as $writerName) {
-            if (isset($classes[$writerName])) {
-                $writers[$writerName] = $c->get($classes[$writerName]);
-            }
-        }
-        return array_values($writers);
+
+        // Use the default formatter
+        $handler->setFormatter($c->get('Piwik\Plugins\Monolog\Formatter\LineMessageFormatter'));
+
+        $logger = new Logger('loginsamlsso', array($handler));
+
+        return $logger;
+
     }),
-
-    'Piwik\Plugins\Monolog\Handler\FileHandler' => DI\object()
-        ->constructor(DI\link('loginsamlsso.log.file.filename'), DI\link('loginsamlsso.log.level'))
-        ->method('setFormatter', DI\link('Piwik\Plugins\Monolog\Formatter\LineMessageFormatter')),
-
-    'Piwik\Plugins\Monolog\Handler\DatabaseHandler' => DI\object()
-        ->constructor(DI\link('loginsamlsso.log.level'))
-        ->method('setFormatter', DI\link('Piwik\Plugins\Monolog\Formatter\LineMessageFormatter')),
-
-    'Piwik\Plugins\Monolog\Handler\WebNotificationHandler' => DI\object()
-        ->constructor(DI\link('loginsamlsso.log.level'))
-        ->method('setFormatter', DI\link('Piwik\Plugins\Monolog\Formatter\LineMessageFormatter')),
 
     'loginsamlsso.log.level' => DI\factory(function (ContainerInterface $c) {
         if ($c->has('ini.LoginSamlSSO.log_level')) {
@@ -73,24 +53,14 @@ return array(
             }
         } else {
             // Default log file
-            $logPath = '/logs/samlsso.log';
+            $logPath = '/logs/login-saml-sso.log';
         }
 
         $logPath = $c->get('path.tmp') . $logPath;
         if (is_dir($logPath)) {
-            $logPath .= '/samlsso.log';
+            $logPath .= '/login-saml-sso.log';
         }
 
         return $logPath;
-    }),
-
-    'Piwik\Plugins\Monolog\Formatter\LineMessageFormatter' => DI\object()
-        ->constructor(DI\link('loginsamlsso.log.format')),
-
-    'loginsamlsso.log.format' => DI\factory(function (ContainerInterface $c) {
-        if ($c->has('ini.LoginSamlSSO.string_message_format')) {
-            return $c->get('ini.LoginSamlSSO.string_message_format');
-        }
-        return '%level% %tag%[%datetime%] %message%';
-    }),
+    })
 );
